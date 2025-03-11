4c8ae44cd29cae9cee4c1914c1df2425a07b9f15
Rodrigo <57541393+Scuero@users.noreply.github.com>
2025-03-11 00:10:21 -0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,6 +2,6 @@ class Solution(object):
     def isPalindrome(self, x):
         if x>=0:
             numero_invertido = int(str(x)[::-1])
-            return ((numero_invertido-x)==0)
+            return (numero_invertido==x)
         return False
         
