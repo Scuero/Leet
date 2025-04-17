04b057a98a0645fe6903c3c3712c39b32f523d03
Rodrigo <57541393+Scuero@users.noreply.github.com>
2025-04-16 21:51:22 -0400

    Time: 4 ms (56.89%) | Memory: 13.8 MB (91.88%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        res = []
+        res = 0
         for numero in nums:
-            if numero not in res:
-                res.append(numero)
-            else:
-                res.remove(numero)
-        return res[0]
-        
+            res = res^numero
+        return res 
