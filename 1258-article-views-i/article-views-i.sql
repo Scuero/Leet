946568f6558ee18e7543eab56aed00bbb70211e0
Rodrigo <57541393+Scuero@users.noreply.github.com>
2025-06-20 23:01:53 -0300

    Time: 339 ms (78.47%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT author_id AS id FROM views GROUP BY author_id, viewer_id HAVING author_id=viewer_id
+SELECT DISTINCT(author_id) AS id FROM views WHERE author_id=viewer_id
